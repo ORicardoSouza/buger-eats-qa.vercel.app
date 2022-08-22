60a2945fee0ca23e5d0c6819572dfd899cc77f34
ORicardoSouza <ricardo.vinicius@outlook.com>
2022-08-22 17:19:31 -0300

    Incluindo projectId

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,9 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'https://buger-eats-qa.vercel.app'
+    baseUrl: 'https://buger-eats-qa.vercel.app',
+    projectId: "8opow8"
+
   },
+
 });
