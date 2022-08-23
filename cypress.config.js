821d8dccbfb74e661f1b0ec707325e611d00dd09
ORicardoSouza <ricardo.vinicius@outlook.com>
2022-08-23 00:42:58 -0300

    Alterando sintaxe do import

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,6 @@
-import { defineConfig } from "cypress";
+const { defineConfig } = require("cypress");
 
-export default defineConfig({
+module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://buger-eats-qa.vercel.app',
     projectId: "8opow8"
